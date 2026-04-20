58682147df23426b7e6b4b1a562246ee6dcdd22f
piljupark <62000236+piljupark@users.noreply.github.com>
2026-04-20 06:45:25 +0000

    웹 테스트 v0.0.8.4

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a54f36a8303e14eb31ca9e5ff3d3fb5c",
+const RESOURCES = {"flutter_bootstrap.js": "dcb03a90ab7cd0a9fa018d40183adaa0",
 "version.json": "a8c43a3fc5622e53b36c4e9b92c96277",
 "index.html": "033a4f218da92fdf94fce1fe49ff9095",
 "/": "033a4f218da92fdf94fce1fe49ff9095",
-"main.dart.js": "8073eb1db76abf5b1986aec922b0087f",
+"main.dart.js": "9ba487e2cb31078715114ade21393121",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "a54f36a8303e14eb31ca9e5ff3d3fb5c",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "5339f28af134c1ed7d4ee0530eecae76",
-"assets/fonts/MaterialIcons-Regular.otf": "61a657fd33719bee6dbf7e42b956d182",
+"assets/fonts/MaterialIcons-Regular.otf": "332e3806f8f5275685a384ca2d07ab33",
 "assets/assets/images/logo_v01.webp": "4675a7e78041fcf6e1d814040d5af9e1",
 "assets/assets/images/backgrounds/log_bg.webp": "155e4b948f8ca50bb5c97f7384d0bf6e",
 "assets/assets/images/backgrounds/bg_login_trainer.webp": "baa639c974c28df1eddb9ba095276fac",
